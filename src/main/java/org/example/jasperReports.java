7ad694ce9a757d3b8de4a8bd133def7d3e828ef6
Joowyy <joelsanchezfernandez05@gmail.com>
2025-12-16 20:50:36 +0100

    Generacion de Informe Metodo en jasperReports.java hecho

--- src/main/java/org/example/jasperReports.java
+++ src/main/java/org/example/jasperReports.java
@@ -3,6 +3,7 @@ package org.example;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -13,12 +14,12 @@ public class jasperReports {
     public static void listarClientesJP_SQL (Connection conexion) {
 
         // Asignamos la ruta de la plantilla, y la ruta de llegada de los informes
-        String Plantilla = "/InformesJasperReports/Plantillas/plantillaClientes.jrxml";
+        String Plantilla = "InformesJasperReports/Plantillas/plantillaClientes.jrxml";
 
         // Informes
-        String InformePDF = "/InformesJasperReports/Informes/InformeClientes.pdf";
-        String InformeXML = "/InformesJasperReports/Informes/InformeClientes.xml";
-        String InformeHTML = "/InformesJasperReports/Informes/InformeClientes.html";
+        String InformePDF = "InformesJasperReports/Informes/InformeClientes.pdf";
+        String InformeXML = "InformesJasperReports/Informes/InformeClientes.xml";
+        String InformeHTML = "InformesJasperReports/Informes/InformeClientes.html";
 
         // Recogemos la fecha actual
         LocalDate fecha =  LocalDate.now();
@@ -34,13 +35,22 @@ public class jasperReports {
         try {
 
             // Vamos casteando la plantilla a JasperReports, y lo compilamos
-            JasperReport jasperReport = JasperCompileManager.compileReport(Plantilla);
+            InputStream is = Thread.currentThread()
+                    .getContextClassLoader()
+                    .getResourceAsStream(Plantilla);
+
+            if (is == null) {
+                throw new RuntimeException("No se encontró la plantilla JRXML");
+            }
+
+            JasperReport jasperReport =
+                    JasperCompileManager.compileReport(is);
 
             // Generamos el Informe a raiz del JasperReports, los parametros asignados y la conexion
             JasperPrint Informe =  JasperFillManager.fillReport(jasperReport, parametros, conexion);
 
             // Esta linea nos permitira ver el informe por pantalla
-            JasperViewer.viewReport(InformePDF, false);
+            JasperViewer.viewReport(Informe, false);
 
             JasperExportManager.exportReportToPdfFile(Informe, InformePDF);
             JasperExportManager.exportReportToHtmlFile(Informe, InformeHTML);
